e37fd37b982b190b98d0cb259b4c201ee4fce2d9
aryansingh2k3 <singh.aryan30004>
2026-07-06 23:02:49 +0530

    Create Todo App layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,39 @@
+import { useState } from "react";
+
 function App() {
+  const [task, setTask] = useState("");
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = () => {
+    if (task === "") {
+      alert("Please Enter Task");
+      return;
+    }
+
+    setTasks([...tasks, task]);
+    setTask("");
+  };
+
   return (
     <>
-     <div>
-    <h1>Todo App</h1>
+      <div>
+        <h1>Todo App</h1>
 
-    <input
-        type="text"
-        placeholder="Enter Task"
-    />
-    <button>Add Task</button>
-</div>
+        <input
+          type="text"
+          placeholder="Enter Task"
+          value={task}
+          onChange={(e) => setTask(e.target.value)}
+        />
+
+        <button onClick={addTask}>Add Task</button>
+
+        <ul>
+          {tasks.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+        </ul>
+      </div>
     </>
   );
 }
