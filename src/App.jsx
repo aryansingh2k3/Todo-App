cb646efade69a79d0cb037397e0337575ce17ed4
aryansingh2k3 <singh.aryan30004>
2026-07-06 15:30:33 +0530

    Add input field and button

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 function App() {
   return (
     <>
-      <h1>Todo App</h1>
+     <div>
+    <h1>Todo App</h1>
+
+    <input
+        type="text"
+        placeholder="Enter Task"
+    />
+    <button>Add Task</button>
+</div>
     </>
   );
 }
